adb27273bc83c437bb5a4e9a80c3a8b27e1d2d30
John! <john.a.roxas@gmail.com>
2023-01-27 07:26:16 -0800

    Create head.js
    Added the head function. Done from Surface, lint check needed 27JAN_23

---
+++ head.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+    console.assert(actual === expected, actual, "!==", expected);
+    if (actual === expected) {
+      console.log("Assertion Passed: ", actual, "===", expected);
+    }
+  };
+
+const head = function (arr) {
+  // Checking if the array that is passed to the function is empty. If so, it will return a value of undefined and terminate immediately.
+  if (arr.length <= 0) {
+    return undefined;
+  }
+  
+  // Assuming the above condition passes, the function simply returns the value at the first index of the array.
+  return arr[0];
+}
+
+// Test cases for the above functions
+/*assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), "Hello");
+assertEqual(head([5]), 5);*/
