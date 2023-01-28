2a7d40e6dc1009d26ec4a50432b8179a0794b128
TheLoneliestTime <john.a.roxas@gmail.com>
2023-01-27 19:47:28 -0800

    wrote functionality of the assertArraysEqual.js code

---
+++ assertArraysEqual.js
@@ -0,0 +1,30 @@
+const eqArrays = function (arr1, arr2) {
+  let test = false;
+  // an initial condition to test whether the lengths of the arrays are similar. If they are have different lengths, they are assumed to be different and the function returns false.
+  if (arr1.length != arr2.length) {
+    return test;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1 [i] === arr2[i]) {
+      test = true;
+    }
+    else {
+      test = false;
+    }
+  }
+  return test;
+}
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log ("These two arrays are equal");
+  }
+  else {
+    console.log ("These two arrays are not equal");
+  }
+}
+
+/*
+assertArraysEqual([1,2,3],[1,2,3]);
+assertArraysEqual([1,2,3],[1,2,4]);*/
