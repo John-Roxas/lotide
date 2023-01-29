a8745e5134296cbb07d6e0bac588816e238757ec
TheLoneliestTime <john.a.roxas@gmail.com>
2023-01-29 10:49:57 -0800

    wrote the functionality of the countLetters.js function

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  console.assert(actual === expected, actual, "!==", expected);
+  if (actual === expected) {
+    console.log(`Assertation Passed: ${actual} === ${expected}`);
+  }
+};
+/* assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual("One", "One"); */
+
+const countLetters = function(toBeCounted) {
+  let count = {};
+  for (const letter of toBeCounted) {
+    if (letter !== " ") {
+      if (count[letter]) {
+        count[letter] += 1;
+      } else {
+        count[letter] = 1;
+      }
+    }
+  }
+  return count;
+};
+
+
